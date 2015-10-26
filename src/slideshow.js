12ccd2cf6b9c077ba406046b1cc9070886ba0337
coder36 <markymiddleton@gmail.com>
2015-10-26 23:15:27 +0000

    Swipe to change slide

--- src/slideshow.js
+++ src/slideshow.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import store from './store'
 import { connect } from 'react-redux';
+import Swipeable from 'react-swipeable'
+
 
 @connect( (state) => state )
 export default class Slideshow extends React.Component {
@@ -18,12 +20,22 @@ export default class Slideshow extends React.Component {
         window.removeEventListener( "keydown", this.keyDownHandler )
     }
 
+    swipeLeft() {
+        store.dispatch({type: "next"});
+    }
+
+    swipeRight() {
+        store.dispatch({type: "prev"});
+    }
+
     render() {
         let slide = React.createFactory(require(`./slides/${this.props.slide}`))(this.props);
         return(
-            <div className='slideShow'>
-                {slide}
-            </div>
+            <Swipeable onSwipingLeft={() => this.swipeLeft()} onSwipingRight={() => this.swipeRight()} className="swipe-container">
+                <div className='slideShow'>
+                    {slide}
+                </div>
+            </Swipeable>
         );
     }
 
